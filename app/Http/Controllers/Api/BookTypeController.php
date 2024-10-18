4f2ca242e5c7cea41ac2486ae8b440b4884dec16
MontherHamdab <montherhammdan2000@gmail.com>
2024-10-18 18:00:09 +0300

    first api

---
+++ app/Http/Controllers/Api/BookTypeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookTypeResource;
+use App\Models\BookType;
+use Illuminate\Http\Request;
+
+class BookTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        return BookTypeResource::collection(BookType::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

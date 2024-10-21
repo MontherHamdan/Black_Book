663faf294dd0a88347d7e50bcb43d20cc42ed08c
MontherHamdab <montherhammdan2000@gmail.com>
2024-10-21 21:35:20 +0300

    first api

--- database/seeders/BookTypeSeeder.php
+++ database/seeders/BookTypeSeeder.php
@@ -6,7 +6,6 @@ use App\Models\BookType;
 use Illuminate\Http\File;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
-use Symfony\Component\HttpKernel\HttpCache\Store;
 
 class BookTypeSeeder extends Seeder
 {
@@ -15,14 +14,14 @@ class BookTypeSeeder extends Seeder
      */
     public function run(): void
     {
-
+        // Store the image in the public disk and get the path
         $imagePath = public_path('images/bookType/book1.png');
-
         $storedImage = Storage::disk('public')->putFile('book_types', new File($imagePath));
 
-        // Generate the URL to the image
-        $imageUrl = Storage::url($storedImage);
+        // Generate the full URL to the image (with domain or IP)
+        $imageUrl = url('storage/' . $storedImage);
 
+        // Insert the data into the database
         BookType::insert([
             [
                 'image' => $imageUrl,
@@ -32,7 +31,7 @@ class BookTypeSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'image' => $storedImage,
+                'image' => $imageUrl,
                 'price' => 13,
                 'description' => 'اهلا-الدفتر سعره-فقط 13 دينار',
                 'created_at' => now(),

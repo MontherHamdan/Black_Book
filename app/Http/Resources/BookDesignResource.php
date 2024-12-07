31738f9e24c68260ddb77c701bd43c5fc703622c
MontherHamdab <montherhammdan2000@gmail.com>
2024-12-07 22:53:30 +0300

    n

--- app/Http/Resources/BookDesignResource.php
+++ app/Http/Resources/BookDesignResource.php
@@ -13,6 +13,7 @@ class BookDesignResource extends JsonResource
             'image' => $this->image ?? 'N/A', // Full image URL
             'category_en' => $this->category->name ?? 'N/A',
             'category_ar' => $this->category->arabic_name ?? 'غير متوفر',
+            'categoryType' => $this->category->type,
             'subcategory_en' => $this->subcategory->name ?? 'N/A',
             'subcategory_ar' => $this->subcategory->arabic_name ?? 'غير متوفر',
             'created_at' => $this->created_at->toDateTimeString(),

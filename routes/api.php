b86801a43b2e8cf87ceb785f92ab7f96141ab370
Monther Hamdan <montherhammdan2000@gmail.com>
2026-05-17 18:31:06 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -65,8 +65,8 @@ Route::prefix('v1')->name('api.')->group(function () {
     Route::get('/discount_codes/check', [DiscountCodeController::class, 'check']);
 
     // *******************User Upload Image ******************************************
-    Route::post('/user_upload_image', [UserImageController::class, 'store'])->middleware('throttle:2000,1');
-    Route::resource('orders', OrderController::class)->only(['store'])->middleware('throttle:1000,1');
+    Route::post('/user_upload_image', [UserImageController::class, 'store'])->middleware('throttle:20,1');
+    Route::resource('orders', OrderController::class)->only(['store'])->middleware('throttle:10,1');
 
     // Diplomas Routes
     Route::resource('diplomas', DiplomaController::class);
